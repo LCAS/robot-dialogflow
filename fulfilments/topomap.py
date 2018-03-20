b18030fc79801e44f8a1d88f8b1c64d757a6f3d9
paul-baxter <pebaxter@gmail.com>
2018-03-20 11:25:48 +0000

    adding collection places and brief descriptions

--- fulfilments/topomap.py
+++ fulfilments/topomap.py
@@ -13,18 +13,73 @@ TOPO_NODES = {
             )
           },
 
-        'plesiosaur': {
-          'x': 400, 'y': 300,
+        'axes': {
+          'x': 85, 'y': 298,
           'description': (
-            "A plesiosaur fossil which was found at the site of "
-            "today's St Faiths Infant School in Lincoln."
+            'These are ancient stone hand axes found in Welton-le-Wold.'
+            )
+          },
+
+        'gold': {
+          'x': 189, 'y': 205,
+          'description': (
+            'Found in Sudbroook and Baumber, these are pieces of gold jewelry from the bronze age.'
             )
           },
 
         'boat': {
           'x': 100, 'y': 50,
           'description': (
-            "An ancient boat."
+            "An ancient boat, made from a single oak tree trunk."
+            )
+          },
+
+        'tombstone': {
+          'x': 297, 'y': 94,
+          'description': (
+            'This is the tombstone of Gaius Valerius, who was buried on what is now the South Common in Lincoln.'
+            )
+          },
+
+        'leadtank': {
+          'x': 439, 'y': 123,
+          'description': (
+            'This is a tank made of lead that shows the existence of Christianity in late Roman Lincolnshire.'
+            )
+          },
+
+        'gravegoods': {
+          'x': 500, 'y': 132,
+          'description': (
+            'These items were found in Anglo Saxon graces around Lincolnshire.'
+            )
+          },
+
+        'bowls': {
+          'x': 550, 'y': 185,
+          'description': (
+            'These bowls are decorative and designed to be hung up, and were found in Anglo Saxon graves in Lincolnshire.'
+            )
+          },
+
+        'matrix': {
+          'x': 648, 'y': 139,
+          'description': (
+            'This matrix is used for sealing documents with wax, and is made from walrus ivory.'
+            )
+          },
+
+        'armour': {
+          'x': 628, 'y': 310,
+          'description': (
+            'This armour and sword is from the Civil War. The sword was found in the river Trent at Gainsborough.'
+            )
+          },
+
+        'plesiosaur': {
+          'x': 455, 'y': 290,
+          'description': (
+            "A plesiosaur fossil which was found at the site of today's St Faiths Infant School in Lincoln."
             )
           },
 
